631a70cb82e1d1fb2f054ebbffb99b29ad828ecc
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-11-08 15:16:08 +0100

    chore(deps): use main branch of eslint-plugin-ember

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 import Application from '@ember/application';
-import config from 'an-app/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
+
+import config from 'an-app/config/environment';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,10 @@
 import { setApplication } from '@ember/test-helpers';
-import Application from 'an-app/app';
-import config from 'an-app/config/environment';
 import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
+
+import Application from 'an-app/app';
+import config from 'an-app/config/environment';
 
 setApplication(Application.create(config.APP));
 
